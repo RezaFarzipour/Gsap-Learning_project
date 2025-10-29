78f8e25782b569014639de11c50120c59e7a2653
Reza <rezafafa78@gmail.com>
2025-10-29 09:46:16 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import gsap from "gsap"
+import { ScrollTrigger,SplitText } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger,SplitText)
+function App() {
+
+
+  return (
+    <>
+      <div>
+      gsap
+      </div>
+      
+    </>
+  )
+}
+
+export default App
